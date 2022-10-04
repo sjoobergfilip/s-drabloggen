ef028fc978023e3bb8fbe177fb9fb85f93650729
Filip Sjöberg <filip.sjoberg@safeture.com>
2022-10-04 21:52:47 +0200

    adding hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../assets/css/main.scss'
+import DanielHero from '../assets/img/daniel.png'
+
+const Hero = () => {
+    return (
+        <div className='hero-container'>
+            <div className='slogan'>
+                <h1>
+                    Tiden Förenar oss
+                </h1>
+                <span>
+                    En plats för all södraiter
+                </span>
+            </div>
+            <div className='image'>
+                <img src={DanielHero} alt="Daniel Ljung" />
+            </div>
+        </div>
+    )
+}
+
+export default Hero
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './Hero'
 import Navbar from './NavBar'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
