20a03b6753c3dbf081cd06c6773e29f98152af5d
Filip Sjöberg <filip.sjoberg@safeture.com>
2022-10-04 21:22:02 +0200

    adding navbar

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './NavBar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../assets/css/navbar.scss'
+import Logo from '../assets/img/södrabloggen.png'
+import {BsInstagram} from 'react-icons/bs'
+import {TbBrandTwitter} from 'react-icons/tb'
+
+const NavBar = () => {
+    return (
+        <div className='navbar-container'>
+            <div >
+                <a href='/'><img className='logga' alt='Logga' src={Logo} /></a>
+            </div>
+            <div className='links'>
+                <a href="/">Nyheter</a>
+                <a href="/">Illustrationer</a>
+                <a href="/">Om Oss</a>
+            </div>
+            <div className='icons'>
+                <a  href="https://twitter.com/sodrabloggen" target="_blank" rel="noreferrer"><TbBrandTwitter /></a>
+                <a href="https://www.instagram.com/sodrabloggen/"  target="_blank" rel="noreferrer"><BsInstagram /></a>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
