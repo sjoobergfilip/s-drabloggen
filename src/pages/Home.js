46744746f1c69492a64837182e19e44939f309dc
Filip Sjöberg <filip.sjoberg@safeture.com>
2022-10-19 11:12:07 +0200

    Adding helmet and dynamic meta content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,24 +7,38 @@ import About from '../components/About'
 import Illustrations from '../components/illustrations'
 import Footer from '../components/Footer'
 import ReactGA from "react-ga4";
+import {Helmet} from "react-helmet";
+import meta_IMG from '../assets/img/meta_BG.png'
 
 
 const Home = () => {
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
-  }, []);
+	useEffect(() => {
+		ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+	}, []);
 
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <NewPost />
-      <MoreNews />
-      <About />
-      <Illustrations />
-      <Footer />
-    </>
-  )
+	return (
+		<div>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Södrabloggen</title>
+				<meta name="description" content="Vi är en oberoende blogg som skriver och följer laget vi älskar J-Södra" />
+				<meta property="og:title" content="Södrabloggen"/>
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content={meta_IMG}/>
+				<meta property="og:url" content={window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search}/>
+				<meta name="twitter:card" content="summary_large_image"/>
+				<meta property="og:description" content="Vi är en oberoende blogg som skriver och följer laget vi älskar J-Södra"/>
+				<meta name="twitter:image:alt" content="SÖdrabloggen.se logga tillsammnas med en illustration av Daniel Ljung"/>
+			</Helmet>
+			<Navbar />
+			<Hero />
+			<NewPost />
+			<MoreNews />
+			<About />
+			<Illustrations />
+			<Footer />
+		</div>
+	)
 }
 
 export default Home
--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -10,6 +10,7 @@ import moment from 'moment'
 import 'moment/locale/sv'
 import ReactGA from "react-ga4";
 import { Share } from 'react-twitter-widgets'
+import {Helmet} from "react-helmet";
 
 
 const builder = imageUrlBuilder(sanityClient);
@@ -21,16 +22,17 @@ const SinglePost = () => {
 	const [singlePost, setSinglePost] = useState(null);
 	const { slug } = useParams();
 	moment.locale('sv');  
-
-useEffect(() => {
-	ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
-    sanityClient
+	useEffect(() => {
+		console.log(window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search)
+		ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
+		sanityClient
     .fetch(
 		`*[slug.current == "${slug}"]{
 			title,
 			_id,
 			slug,
 			publishedAt,
+			ingress,
 			mainImage{
 				asset->{
 					_id,
@@ -50,7 +52,19 @@ useEffect(() => {
 
 	if (!singlePost) return <div>Loading...</div>;
 	return (
-		<>
+		<div>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>{'Södrabloggen - ' + singlePost.title}</title>
+				<meta name="description" content="Vi är en oberoende blogg som skriver och följer laget vi älskar J-Södra" />
+				<meta property="og:title" content={singlePost.title}/>
+				<meta property="og:type" content="article" />
+				<meta property="og:image" content={singlePost.mainImage.asset.url}/>
+				<meta property="og:url" content={window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search}/>
+				<meta name="twitter:card" content="summary_large_image"/>
+				<meta property="og:description" content={singlePost.ingress}/>
+				<meta name="twitter:image:alt" content="This is the image that we have on the blogbost"/>
+			</Helmet>
 			<NavbarNews />
 			<main>
 				<LazyHero color="#0C2309" imageSrc={singlePost.mainImage.asset.url}></LazyHero>
@@ -63,8 +77,7 @@ useEffect(() => {
 							<img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name}/>
 							<a href={singlePost.twitterLink}>{singlePost.twitterName}</a>
 						</div>
-						<p>{moment().format('Do MMMM YYYY',singlePost.publishedAt) }</p>
-						{/* <Share url={'https://dev.twitter.com/web/tweet-button'} options={{ size: "large" }} /> */}
+						<p>{moment(singlePost.publishedAt).format('Do MMMM YYYY') }</p>
 						<Share url={`https://sodrabloggen.se${window.location.pathname}`} options={{ size: "large" }} />
 					</div>
 					
@@ -76,7 +89,7 @@ useEffect(() => {
 				</div>
 			</main>
 			<Footer />
-		</>
+		</div>
 	)
 }
 
