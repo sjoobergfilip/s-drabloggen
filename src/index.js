ab790303de504b05146a501ca19a3ceecf8a360c
Filip Sjöberg <filip.sjoberg@safeture.com>
2022-11-18 11:38:53 +0100

    remove react snap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { hydrate, render } from "react-dom";
 
-const APP = (
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    APP, rootElement);
-} else {
-  render(
-    APP, rootElement);
-}
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
